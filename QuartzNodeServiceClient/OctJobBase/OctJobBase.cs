5b0ff8275217abfcf2478d7dba507156ef070049
gardiok <gardiolaken@gmail.com>
2026-02-28 18:57:08 -0500

    Apply settings

---
+++ QuartzNodeServiceClient/OctJobBase/OctJobBase.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Data.SqlClient;
+using Quartz;
+using System.Reflection;
+
+namespace QuartzNodeServiceClient
+{
+	public abstract class OctJobBase : IJob
+	{
+		private readonly ILogger _logger;
+		protected IConfiguration _configuration;
+		public OctJobBase(ILogger logger, IConfiguration config)
+		{
+			_configuration = config;
+			_logger = logger;
+		}
+
+		public Task Execute(IJobExecutionContext context)
+		{
+			Process();
+			return Task.CompletedTask;
+		}
+
+		protected abstract void Process();
+
+		protected string GetConfig()
+		{
+			var connectionString = _configuration.GetConnectionString("QuartzConnectionString");
+			var settingsKey = _configuration.GetValue<string>("QuartzSettingsKey");
+
+			if (string.IsNullOrWhiteSpace(settingsKey))
+			{
+				_logger.LogWarning("No settings key provided.");
+				return string.Empty;
+			}
+
+			var configValue = string.Empty;
+			try
+			{
+				var sql = @"
+        SELECT TOP 1 Settings
+        FROM [Quartz].[dbo].[Settings]
+		WHERE
+		[Key] = @SettingsKey
+";
+				using var conn = new SqlConnection(connectionString);
+				using var cmd = new SqlCommand(sql, conn);
+				cmd.Parameters.AddWithValue("@SettingsKey", settingsKey);
+
+				conn.Open();
+				using var reader = cmd.ExecuteReader();
+
+				if (reader.Read())
+				{
+					configValue = reader.GetString(reader.GetOrdinal("Settings"));
+				}
+			}
+			catch (Exception ex)
+			{
+				_logger.LogError($"Error retrieving config for key {settingsKey}: {ex.Message}");
+			}
+
+			return configValue;
+		}
+	}
+	
+}
--- SampleJob/SampleJob.cs
+++ SampleJob/SampleJob.cs
@@ -1,13 +1,15 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using Quartz;
+using QuartzNodeServiceClient;
 
 namespace SampleJob
 {
-	public class FileImporter : IJob
+	public class FileImporter : OctJobBase
 	{
 		private ILogger<FileImporter> _logger;
 
-		public FileImporter(ILogger<FileImporter> logger)
+		public FileImporter(ILogger<FileImporter> logger, IConfiguration config) : base(logger, config)
 		{
 			_logger = logger;
 		}
@@ -16,6 +18,11 @@ namespace SampleJob
 			_logger.LogInformation("SampleJob is executing at {time}", DateTimeOffset.Now);
 			File.WriteAllBytes(@"C:\DATA\SampleJobExecuted.txt", System.Text.Encoding.UTF8.GetBytes($"SampleJob executed at {DateTimeOffset.Now}"));
 			return Task.CompletedTask;
+		}
+
+		protected override void Process()
+		{
+			throw new NotImplementedException();
 		}
 	}
 }
